194b3f08a6a9447478ab275cf7128eeb6d274afc
Steven Oyer <contact.stevenoyer@gmail.com>
2023-12-03 15:36:05 +0100

    Creating documentation with Nelmio

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -12,12 +12,56 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 
 #[Route('/api')]
 class PhoneController extends AbstractController
 {
     /**
      * Get all phones
+     * 
+     * @OA\Response(
+     *     response=200,
+     *     description="Retourne la liste des téléphones",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Phone::class))
+     *     )
+     * )
+     * @OA\Parameter(
+     *     name="page",
+     *     in="query",
+     *     description="La page que l'on veut récupérer",
+     *     @OA\Schema(type="int")
+     * )
+     *
+     * @OA\Parameter(
+     *     name="limit",
+     *     in="query",
+     *     description="Le nombre d'éléments que l'on veut récupérer",
+     *     @OA\Schema(type="int")
+     * )
+     * 
+     * @OA\Response(
+     *     response = 401,
+     *     description = "Vous n'êtes pas authentifié."
+     * )
+     * 
+     * @OA\Response(
+     *     response = 404,
+     *     description = "Cette ressource n'existe pas."
+     * )
+     * 
+     * @OA\Tag(name="Phone")
+     * 
+     * @param PhoneRepository $phoneRepository
+     * @param Request $request
+     * @param TagAwareCacheInterface $cache
+     * @param SerializerInterface $serializer
+     * 
+     * @return JsonResponse
      */
     #[Route('/phones', name: 'app_phones', methods: ['GET'])]
     #[IsGranted('ROLE_CUSTOMER', message: 'Vous devez être authentifié en tant que Client pour accéder à cette ressource.')]
@@ -39,8 +83,45 @@ class PhoneController extends AbstractController
         return new JsonResponse($jsonPhoneList, JsonResponse::HTTP_OK, [], true);
     }
 
+
     /**
      * Get detail phone
+     * 
+     * @OA\Response(
+     *     response=200,
+     *     description="Retourne le détail d'un téléphone",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Phone::class))
+     *     )
+     * )
+     * 
+     * @OA\Parameter(
+     *    name="id",
+     *    in="path",
+     *    description="Id du téléphone",
+     *    required=true,
+     *    @OA\Schema(
+     *        type="integer"
+     *    )
+     *  )
+     * 
+     * @OA\Response(
+     *     response = 401,
+     *     description = "Vous n'êtes pas authentifié."
+     * )
+     * 
+     * @OA\Response(
+     *     response = 404,
+     *     description = "Cette ressource n'existe pas."
+     * )
+     * 
+     * @OA\Tag(name="Phone")
+     * 
+     * @param Phone $phone
+     * @param SerializerInterface $serializer
+     * 
+     * @return JsonResponse
      */
     #[Route('/phones/{id}', name: 'app_detail_phone', methods: ['GET'])]
     #[IsGranted('ROLE_CUSTOMER', message: 'Vous devez être authentifié en tant que Client pour accéder à cette ressource.')]
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,24 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use OpenApi\Annotations as OA;
+use App\Service\CustomerService;
+use App\Repository\UserRepository;
 use App\Exception\ForbiddenException;
 use App\Repository\CustomerRepository;
-use App\Repository\UserRepository;
-use App\Service\CustomerService;
+use JMS\Serializer\SerializerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializationContext;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use JMS\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Contracts\Cache\ItemInterface;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/api')]
 class UserController extends AbstractController
@@ -54,8 +58,53 @@ class UserController extends AbstractController
         $this->validator = $validator;
     }
 
+
     /**
-     * Get all uers
+     * Get all users
+     * 
+     * @OA\Response(
+     *     response=200,
+     *     description="Retourne la liste des utilisateurs du client authentifié",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=User::class, groups={"getUsers"}))
+     *     )
+     * )
+     * @OA\Parameter(
+     *     name="page",
+     *     in="query",
+     *     description="La page que l'on veut récupérer",
+     *     @OA\Schema(type="int")
+     * )
+     *
+     * @OA\Parameter(
+     *     name="limit",
+     *     in="query",
+     *     description="Le nombre d'éléments que l'on veut récupérer",
+     *     @OA\Schema(type="int")
+     * )
+     * 
+     * @OA\Response(
+     *     response = 401,
+     *     description = "Vous n'êtes pas authentifié."
+     * )
+     * 
+     * @OA\Response(
+     *     response = 403,
+     *     description = "Vous n'avez pas accès à cette ressource."
+     * )
+     * 
+     * @OA\Response(
+     *     response = 404,
+     *     description = "Cette ressource n'existe pas."
+     * )
+     * 
+     * @OA\Tag(name="User")
+     * 
+     * @param Request $request
+     * @param TagAwareCacheInterface $cache
+     * 
+     * @return JsonResponse
      */
     #[Route('/users', name: 'app_users', methods: ['GET'])]
     #[IsGranted('ROLE_CUSTOMER', message: 'Vous devez être authentifié en tant que Client pour accéder à cette ressource.')]
@@ -81,8 +130,50 @@ class UserController extends AbstractController
         return new JsonResponse($jsonUsersList, JsonResponse::HTTP_OK, [], true);
     }
 
+
     /**
      * Get user by id
+     * 
+     * @OA\Response(
+     *     response=200,
+     *     description="Retourne le détail d'un utilisateur du client authentifié",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=User::class, groups={"getUsers"}))
+     *     )
+     * )
+     * 
+     * @OA\Parameter(
+     *    name="id",
+     *    in="path",
+     *    description="Id de l'utilisateur",
+     *    required=true,
+     *    @OA\Schema(
+     *        type="integer"
+     *    )
+     *  )
+     * 
+     * @OA\Response(
+     *     response = 401,
+     *     description = "Vous n'êtes pas authentifié."
+     * )
+     * 
+     * @OA\Response(
+     *     response = 403,
+     *     description = "Vous n'avez pas accès à cette ressource."
+     * )
+     * 
+     * @OA\Response(
+     *     response = 404,
+     *     description = "Cette ressource n'existe pas."
+     * )
+     * 
+     * @OA\Tag(name="User")
+     * 
+     * @param User $user
+     * @param Request $request
+     * 
+     * @return JsonResponse
      */
     #[Route('/users/{id}', name: 'app_detail_user', methods: ['GET'])]
     #[IsGranted('ROLE_CUSTOMER', message: 'Vous devez être authentifié en tant que Client pour accéder à cette ressource.')]
@@ -98,8 +189,41 @@ class UserController extends AbstractController
         return new JsonResponse($jsonUser, JsonResponse::HTTP_FOUND, [], true);
     }
 
+
     /**
      * Create user
+     * 
+     * @OA\Response(
+     *     response=201,
+     *     description="Création d'un utilisateur d'un client authentifié",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=User::class, groups={"getUsers"}))
+     *     )
+     * )
+     * 
+     * @OA\Response(
+     *     response = 401,
+     *     description = "Vous n'êtes pas authentifié."
+     * )
+     * 
+     * @OA\Response(
+     *     response = 403,
+     *     description = "Vous n'avez pas accès à cette ressource."
+     * )
+     * 
+     * @OA\Response(
+     *     response = 404,
+     *     description = "Cette ressource n'existe pas."
+     * )
+     * 
+     * @OA\RequestBody(@Model(type=User::class, groups={"createUser"}))
+     * @OA\Tag(name="User")
+     * 
+     * @param User $user
+     * @param Request $request
+     * 
+     * @return JsonResponse
      */
     #[Route('/users', name: 'app_create_user', methods: ['POST'])]
     #[IsGranted('ROLE_CUSTOMER', message: 'Vous devez être authentifié en tant que Client pour accéder à cette ressource.')]
@@ -112,10 +236,11 @@ class UserController extends AbstractController
 
         /* Get content in table form */
         $content = $request->toArray();
-        $customerId = $content['customer_id'] ?? -1;
+        //$customerId = $content['customer_id'] ?? -1;
+        $customer = $this->customerService->getCustomerByToken($request);
 
         /* Set find customer by customer_id in content */
-        $user->setCustomer($this->customerRepository->find($customerId));
+        $user->setCustomer($customer);
         /* Define password hash by password in content */
         $user->setPassword($this->userPasswordHasher->hashPassword($user, $content['password']));
 
@@ -140,8 +265,46 @@ class UserController extends AbstractController
         return new JsonResponse($jsonUser, JsonResponse::HTTP_CREATED, ['Location' => $location], true);
     }
 
+
     /**
      * Delete user by id
+     * 
+     * @OA\Response(
+     *     response=204,
+     *     description="Supprime un utilisateur par son id d'un client authentifié."
+     * )
+     * 
+     * @OA\Response(
+     *     response = 401,
+     *     description = "Vous n'êtes pas authentifié."
+     * )
+     * 
+     * @OA\Response(
+     *     response = 403,
+     *     description = "Vous n'avez pas accès à cette ressource."
+     * )
+     * 
+     * @OA\Response(
+     *     response = 404,
+     *     description = "Cette ressource n'existe pas."
+     * )
+     * 
+     * @OA\Parameter(
+     *    name="id",
+     *    in="path",
+     *    description="Id de l'utilisateur",
+     *    required=true,
+     *    @OA\Schema(
+     *        type="integer"
+     *    )
+     *  )
+     * 
+     * @OA\Tag(name="User")
+     * 
+     * @param User $user
+     * @param Request $request
+     * 
+     * @return JsonResponse
      */
     #[Route('/users/{id}', name: 'app_delete_user', methods: ['DELETE'])]
     #[IsGranted('ROLE_CUSTOMER', message: 'Vous devez être authentifié en tant que Client pour accéder à cette ressource.')]
